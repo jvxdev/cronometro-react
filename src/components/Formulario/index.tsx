b0be4ebbcba44a2fbe6ae57df8f5a505764783de
João Victor Dias de Moraes <joaovictorddm@outlook.com>
2023-06-05 10:06:15 -0300

    Utilização da prop children no componente Botao para alteração do nome do botão.

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -19,16 +19,18 @@ class Formulario extends React.Component {
                 <div className={style.inputContainer}>
                     <label htmlFor="tempo">Tempo</label>
                     <input type="time"
-                    step="1"
-                    name="tempo"
-                    id="tempo"
-                    min="00:00:00"
-                    max="01:30:00"
-                    required
+                        step="1"
+                        name="tempo"
+                        id="tempo"
+                        min="00:00:00"
+                        max="01:30:00"
+                        required
                     />
                 </div>
 
-                <Botao />
+                <Botao>
+                    Salvar
+                </Botao>
             </form>
         )
     }
